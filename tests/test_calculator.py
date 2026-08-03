6a44ff52976fc18df46cf357c40cba5356e98764
Shrirang Awaghad <shriawaghad@gmail.com>
2026-08-03 19:42:17 +0530

    Format calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,17 @@
-from calculator import add
+from calculator import add, subtract, multiply, divide
 
 
 def test_add():
     assert add(2, 3) == 5
+
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+
+
+def test_multiply():
+    assert multiply(4, 5) == 20
+
+
+def test_divide():
+    assert divide(10, 2) == 5
